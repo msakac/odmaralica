22d13f08962181786daef8a983b94d1e0a2c63f3
msakac <msakac98@gmaIl.com>
2023-08-22 21:37:25 +0200

    feat(cors): add frontend as allowed origin

--- Backend/src/main/java/org/foi/diplomski/msakac/odmaralica/Odmaralica.java
+++ Backend/src/main/java/org/foi/diplomski/msakac/odmaralica/Odmaralica.java
@@ -2,8 +2,11 @@ package org.foi.diplomski.msakac.odmaralica;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableJpaRepositories
@@ -14,4 +17,13 @@ public class Odmaralica {
         SpringApplication.run(Odmaralica.class, args);
     }
 
+    @Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+			}
+		};
+	}
 }
