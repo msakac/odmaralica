5c8d9f8bc42804ec7f17137ba32f12ac0581bbf4
msakac <msakac98@gmaIl.com>
2023-08-28 09:02:14 +0200

    git(fix)

--- Backend/src/main/java/org/foi/diplomski/msakac/odmaralica/security/oauth/OAuth2Config.java
+++ Backend/src/main/java/org/foi/diplomski/msakac/odmaralica/security/oauth/OAuth2Config.java
@@ -1,5 +1,6 @@
 package org.foi.diplomski.msakac.odmaralica.security.oauth;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
@@ -8,12 +9,34 @@ import org.springframework.security.oauth2.client.registration.InMemoryClientReg
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 @Configuration
+@ConfigurationProperties(prefix = "oauth2-google")
 public class OAuth2Config {
+
+    private String clientId;
+    private String clientSecret;
+    private String redirectUri;
 
     @Bean
     public ClientRegistrationRepository clientRegistrationRepository() {
         ClientRegistration clientRegistration = ClientRegistration.withRegistrationId("google")
+                .clientId(clientId)
+                .clientSecret(clientSecret)
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .redirectUri(redirectUri)
+                .scope("email", "profile")
+                .authorizationUri("https://accounts.google.com/o/oauth2/v2/auth")
+                .tokenUri("https://www.googleapis.com/oauth2/v4/token")
+                .userInfoUri("https://www.googleapis.com/oauth2/v3/userinfo")
+                .userNameAttributeName("email")
+                .clientName("Google")
+                .build();
 
         InMemoryClientRegistrationRepository clientRegistrationRepository = new InMemoryClientRegistrationRepository(clientRegistration);
 
